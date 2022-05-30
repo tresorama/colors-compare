1da0cdcc8d86d38d2a151803bfdc04f97d39c154
Jacopo Marrone <jacopo.marrone27@gmail.com>
2022-05-30 22:34:22 +0200

    Refractor: Webpack: - added full production webpack config - now css in production is a separate file - added babel to transpile js

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,95 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const CopyPlugin = require("copy-webpack-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 
 module.exports = {
-  mode: 'development',
-  devtool: 'source-map',
-  devServer: {
-    static: './dist',
-    watchFiles: ['src/**/*'],
-  },
-  entry: './src/js/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
+    mode: 'development',
+    devtool: 'source-map',
+    // Webpack dev server configuration
+    devServer: {
+        static: './dist',
+        watchFiles: ['src/**/*'],
+    },
+    // Webpack entry point ( INPUT )
+    entry: './src/js/index.js',
+    // Webpack output configuration ( OUTPUT )
+    output: {
+        filename: 'main.[hash].js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            // When webpack encounters a .css files
+            {
+                test: /\.css$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader"
+                ],
+            },
+            // When webpack encounters a .sass|scss file ...
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Injects CSS to the DOM via JS (good in development)
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader",
+                ],
+            },
+            // When webpack encounters a .js|jsx file...
+            {
+                test: /\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: [
+                            // Convert new ES6 features to old ES5
+                            [
+                                "@babel/preset-env",
+                                // Enable usage of js async/awaits
+                                {
+                                    "useBuiltIns": "usage",
+                                    "corejs": 3,
+                                    "targets": "defaults"
+                                }
+                            ],
+                            // Convert JSX to normal JS
+                            // "@babel/preset-react"
+                        ],
+                    }
+                }
+            },
+            // When webpack encounters a .pug file ...
+            // {
+            //   test: /\.pug$/,
+            //   loader: '@webdiscus/pug-loader',
+            // },
+        ],
+    },
+    plugins: [
+        // Copy 'src/public' folder contents to 'dist', to be served as static files
+        new CopyPlugin({
+            patterns: [
+                { from: "src/public", to: "." },
+            ],
+        }),
+        // Use "HTML Template" when building html files
+        // Webpack injects the JS and CSS files into the HTML file
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'src/index.html'
+        }),
+        // new HtmlWebpackPlugin({
+        //   filename: 'about-us.html',
+        //   template: 'src/pages/about.html',
+        // }),
     ],
-  },
-  plugins: [
-    // Copy 'src/public' folder contents to 'dist', to be served as static files
-    // new CopyPlugin({
-    //   patterns: [
-    //     { from: "src/public", to: "." },
-    //   ],
-    // }),
-    // Copy html pages to 'dist'
-    new HtmlWebpackPlugin({
-      filename: 'index.html',
-      template: 'src/index.html'
-    }),
-    // new HtmlWebpackPlugin({
-    //   filename: 'calcola-guadagno.html',
-    //   template: 'src/pages/calcola-guadagno.html',
-    // }),
-    // new HtmlWebpackPlugin({
-    //   filename: 'oggetti-che-si-vendono-velocemente.html',
-    //   template: 'src/pages/oggetti-che-si-vendono-velocemente.html',
-    // }),
-  ],
 
 };
